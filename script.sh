a32f4d9d19f753245bf23e6981a4f7990012aa1d
daviddbyrd <151666012+daviddbyrd@users.noreply.github.com>
2024-03-03 13:35:48 +0000

    added job completed alert

--- script.sh
+++ script.sh
@@ -4,7 +4,13 @@ rm -r fol_1 fol_2
 mkdir fol_1 fol_2
 
 cd ./fol_1
-touch "1_1.txt" "1_2.txt" "1_3.txt"
+touch 1_1.txt 1_2.txt 1_3.txt
+chmod 600 1_1.txt 1_3.txt
+chmod 777 1_2.txt 
 
 cd ../fol_2
 touch "2_1.txt" "2_2.txt" "2_3.txt"
+chmod 600 2_1.txt 2_3.txt
+chmod 777 2_2.txt 
+
+echo "Job completed"
